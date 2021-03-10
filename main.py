11ddb8a1fe6e787e93b7cb9cfb6c92e460f371eb
TurBoss <j.l.toledano.l@gmail.com>
2021-03-11 00:39:34 +0100

    remove all the sql stuff from main

--- main.py
+++ main.py
@@ -4,40 +4,14 @@ import discord
 from yaml import load
 from yaml import CLoader as Loader, CDumper as Dumper
 
-from sqlalchemy import create_engine, select, and_, text
-from sqlalchemy import Table, Column, Boolean, Integer, String, MetaData, Date, Time
 
-
-class TurBoEventClient(discord.Client):
+class OpenSethClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.bot_ready = False
         
-        self.meta = MetaData()
-
-        self.event_table = Table('events', self.meta,
-                                 Column('id', Integer, primary_key = True),
-                                 Column('event', Integer),
-                                 Column('name', String),
-                                 Column('date', Date),
-                                 Column('hour', Time)
-                                 )
-
-        self.user_table = Table('users', self.meta,
-                                 Column('id', Integer, primary_key = True),
-                                 Column('event', Integer),
-                                 Column('name', String),
-                                 Column('member_id', Integer),
-                                 Column('active', Boolean)
-                                 )
-        
-        self.engine = create_engine('sqlite:///events.db')
-        self.meta.create_all(self.engine)
-        
-        self.conn = self.engine.connect()
-        
-        self.message_id = 813882134250389586
+        self.message_id = 819329888695615528
         
 
     async def on_ready(self):
@@ -59,14 +33,8 @@ class TurBoEventClient(discord.Client):
         elif message.content.startswith('!create'):
             data = re.findall('"([^"]*)"', message.content)
             if len(data) == 3:
-                ins = self.event_table.insert().values(event=int(message_id),
-                                               name=data[0],
-                                               date=data[1],
-                                               hour=data[2],
-                                               active=True)
-                result = self.conn.execute(ins)
                 await message.reply('New event created: {} on {} at {}'.format(data[0], data[1], data[2]), mention_author=True)
-            
+        
         elif message.content.startswith('!help'):
             await message.reply('Create a new event\nExample: "!create "World Event" "31/03/2021" "18:30"', mention_author=True)
 
@@ -82,32 +50,10 @@ class TurBoEventClient(discord.Client):
         if message_id != self.message_id:
             return
         
-        s = self.user_table.select().where(
-            and_(
-                text("member_id == :user_id"),
-                text("active == 1")
-            )
-        )
-        
-        result = self.conn.execute(s, user_id=payload.user_id).fetchall()
-        
-        print(result)
-        
-        if result:
-            print("User {} already registered".format(member))
-            return
-        print("Register user {} for event {}".format(member, message_id))
-        
-        ins = self.user_table.insert().values(event=int(message_id),
-                                               name=str(member),
-                                               member_id=str(user_id),
-                                               active=True)
-        result = self.conn.execute(ins)
-
     async def on_raw_reaction_remove(self, payload):
         if not self.bot_ready:
             return
-                
+        
         message_id = payload.message_id
         member = payload.member
         user_id = payload.user_id
@@ -122,20 +68,9 @@ class TurBoEventClient(discord.Client):
             )
         )
         
-        result = self.conn.execute(s, user_id=user_id).fetchall()
-        
-        if not result:
-            print("User {} not registered".format(member))
-            return
-        
-        stmt = self.user_table.update().where(text("member_id == :user_id")).values(active=False)
-        
-        self.conn.execute(stmt, user_id=user_id)
-        
         print("Remove notification for user_id {}".format(user_id))
-        
 
-        
+
 # This bot requires the members and reactions intents.
 intents = discord.Intents.default()
 intents.members = True
@@ -145,5 +80,5 @@ with open("config.yml") as f:
 
 token = config["bot"]["token"]
 
-client = TurBoEventClient(intents=intents)
+client = OpenSethClient(intents=intents)
 client.run(token)
